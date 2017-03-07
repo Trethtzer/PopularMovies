a29b61fd3ec048a1bb6b89787cccbe6259ad31ae
Trethtzer <joker.crimson@gmail.com>
2017-03-07 12:52:48 +0100

    PM 2.1
    Ready to start programming

---
+++ app/src/main/java/app/com/trethtzer/popularmovies/utilities/Utility.java
@@ -0,0 +1,10 @@
+package app.com.trethtzer.popularmovies.utilities;
+
+/**
+ * Created by Trethtzer on 07/03/2017.
+ *
+ * Esta clase contiene funciones auxiliares.
+ */
+
+public class Utility {
+}
